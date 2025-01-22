ce806bf135fa12ed3005f735f205f684cb619265
Troya A <aetroyab@uce.edu.ec>
2025-01-21 20:38:01 -0500

    pokedex demo

---
+++ src/main/java/ec/edu/uce/DemoPokedex/DemoPokedexApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.DemoPokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPokedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/uce/DemoPokedex/DemoPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.DemoPokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
