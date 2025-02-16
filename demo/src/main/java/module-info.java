36ddffd003aa31ea94a0abcc63992dab7f891ffd
Troya A <aetroyab@uce.edu.ec>
2025-02-16 13:36:11 -0500

    commit

--- demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ec.edu.uce.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ec.edu.uce.demo to javafx.fxml;
-    exports ec.edu.uce.demo;
-}
--- src/main/java/ec/edu/uce/DemoPokedex/ApiService/PokeService.java
+++ src/main/java/ec/edu/uce/DemoPokedex/ApiService/PokeService.java
@@ -5,11 +5,8 @@ import ec.edu.uce.DemoPokedex.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
